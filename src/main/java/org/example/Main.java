bb817f14131032d6f9df1e1ea76895dd17fe44d4
ved603 <gujjuvedant603@gmail.com>
2024-04-30 18:13:19 +0530

    The Code is Updated with Date Matching feature

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,22 +1,29 @@
 package org.example;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
 class Hotels {
     String Name;
     long Regular_Price;
-
-    Hotels(String name, long regular_price) {
+    String start;
+    String end;
+    Hotels(String name, long regular_price,String start,String end) {
         this.Name = name;
         this.Regular_Price = regular_price;
+        this.start = start;
+        this.end = end;
     }
 
     @Override
     public String toString() {
         System.out.println("The Name of the Hotel is : " + Name);
         System.out.println("The Regular Price of an Hotel is : " + Regular_Price);
+        System.out.println("The Start Date of the Hotel is : " +start);
+        System.out.println("The end Date of the Hotel is : " +end);
         return "";
     }
 }
@@ -24,8 +31,25 @@ public class Main {
     static List<Hotels> hotelList = new ArrayList<>();
     static Scanner sc = new Scanner(System.in);
 
-    static Boolean addHotel(String name, long regular_price) {
-        Hotels hot = new Hotels(name, regular_price);
+    static String cheapest(String Start,String end) {
+        long min= Integer.MAX_VALUE;
+//        System.out.println(min);
+        String name = null;
+
+        for (Hotels hotels : hotelList) {
+            if(Start.equals(hotels.start) && end.equals(hotels.end)) {
+                if (hotels.Regular_Price < min) {
+                    min = hotels.Regular_Price;
+                    name = hotels.Name;
+
+                }
+            }
+        }
+
+        return name;
+    }
+    static Boolean addHotel(String name, long regular_price,String start,String end) {
+        Hotels hot = new Hotels(name, regular_price, start, end );
         hotelList.add(hot);
         if(name.equals(hot.Name) && regular_price == hot.Regular_Price) {
             return true;
@@ -33,7 +57,14 @@ public class Main {
         else{
             return false;
         }
+    }
 
+    public static void main(String args[]){
+        Main m = new Main();
+        m.addHotel("LakeView",20000,"20-may-2002","21-july-2002");
+        m.addHotel("TajLand Ends",4000,"20-may-2002","21-july-2002");
+        m.addHotel("LakePlacids",5000,"20-jan-2002","11-feb-2002");
+        m.addHotel("TajLand Ends",4000,"22-nov-2002","22-dec-2002");
 
     }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -5,10 +5,23 @@ import org.junit.Test;
 
 
 public class MainTest {
+    Main m = new Main();
     @Test
     public void testAdd(){
-        Main m = new Main();
-        Assert.assertTrue(m.addHotel("LakeView",20000));
-        Assert.assertTrue(  m.addHotel("TajLand Ends",4000));
+
+        Assert.assertTrue(m.addHotel("LakeView",20000,"20-may-2002","21-july-2002"));
+        Assert.assertTrue(  m.addHotel("TajLand Ends",4000,"20-may-2002","21-july-2002"));
+        Assert.assertTrue(  m.addHotel("LakePlacids",5000,"20-jan-2002","11-feb-2002"));
+        Assert.assertTrue(  m.addHotel("TajLand Ends",4000,"22-nov-2002","22-dec-2002"));
+    }
+
+    @Test
+    public void testcheapest(){
+        m.addHotel("LakeView",20000,"20-may-2002","21-july-2002");
+        m.addHotel("TajLand Ends",4000,"20-may-2002","21-july-2002");
+        m.addHotel("LakePlacids",5000,"20-jan-2002","11-feb-2002");
+        m.addHotel("TajLand Ends",4000,"22-nov-2002","22-dec-2002");
+        String str = m.cheapest("20-may-2002","21-july-2002");
+        Assert.assertEquals("TajLand Ends",str);
     }
 }
