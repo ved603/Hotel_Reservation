da1f00970ce404d485ee5584091aa61b5ad6464f
ved603 <164326875+ved603@users.noreply.github.com>
2024-05-14 17:59:59 +0530

    Rating has been Added

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -9,18 +9,18 @@ public class MainTest {
     @Test
     public void testAdd(){
 
-        Assert.assertTrue(m.addHotel("LakeView",20000,"20-may-2002","21-july-2002","26-may-2002","27-may-2002",30000));
-        Assert.assertTrue(  m.addHotel("TajLand Ends",4000,"20-may-2002","21-july-2002","26-may-2002","27-may-2002",6000));
-        Assert.assertTrue(  m.addHotel("LakePlacids",5000,"20-jan-2002","11-feb-2002","25-jan-2002","26-jan-2002",7000));
-        Assert.assertTrue(  m.addHotel("TajLand Ends",4000,"22-nov-2002","22-dec-2002","10-dec-2002","11-dec-2002",5000));
+        Assert.assertTrue(m.addHotel("LakeWood",20000,"20-may-2002","21-july-2002","26-may-2002","27-may-2002",30000,3));
+        Assert.assertTrue(  m.addHotel("BridgeWood",4000,"20-may-2002","21-july-2002","26-may-2002","27-may-2002",6000,4));
+        Assert.assertTrue(  m.addHotel("RidgeWood",5000,"20-jan-2002","11-feb-2002","25-jan-2002","26-jan-2002",7000,5));
+
     }
 
     @Test
     public void testcheapest(){
-        m.addHotel("LakeView",20000,"20-may-2002","21-july-2002","26-may-2002","27-may-2002",30000);
-        m.addHotel("TajLand Ends",4000,"20-may-2002","21-july-2002","26-may-2002","27-may-2002",6000);
-        m.addHotel("LakePlacids",5000,"20-jan-2002","11-feb-2002","25-jan-2002","26-jan-2002",7000);
-        m.addHotel("TajLand Ends",4000,"22-nov-2002","22-dec-2002","10-dec-2002","11-dec-2002",5000);
+
+        Assert.assertTrue(m.addHotel("LakeWood",20000,"20-may-2002","21-july-2002","26-may-2002","27-may-2002",30000,3));
+        Assert.assertTrue(  m.addHotel("BridgeWood",4000,"20-may-2002","21-july-2002","26-may-2002","27-may-2002",6000,4));
+        Assert.assertTrue(  m.addHotel("RidgeWood",5000,"20-jan-2002","11-feb-2002","25-jan-2002","26-jan-2002",7000,5));
         String str = m.cheapest("20-may-2002","21-july-2002","26-may-2002","27-may-2002");
         Assert.assertEquals("TajLand Ends",str);
     }
