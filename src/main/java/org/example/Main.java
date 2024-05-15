2cc3b4cebd69f5b1e6480e60b03f7fefcdac7aff
ved603 <164326875+ved603@users.noreply.github.com>
2024-05-15 12:00:56 +0530

    Added the rate for rated customer

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,9 +14,10 @@ class Hotels {
     String start_week;
     String end_week;
     int Rating;
-
+    long srr;
+    long swr;
     long week_price;
-    Hotels(String name, long regular_price,String start,String end,String start_week,String end_week,long week_price,int Rating) {
+    Hotels(String name, long regular_price,String start,String end,String start_week,String end_week,long week_price,int Rating,long srr, long swr) {
         this.Name = name;
         this.Regular_Price = regular_price;
         this.start = start;
@@ -25,6 +26,8 @@ class Hotels {
         this.end_week = end_week;
         this.week_price = week_price;
         this.Rating = Rating;
+        this.srr = srr;
+        this.swr = swr;
     }
 
     @Override
@@ -42,30 +45,44 @@ class Hotels {
 }
 public class Main {
     static List<Hotels> hotelList = new ArrayList<>();
+    static ArrayList<Integer> arr = new ArrayList<Integer>();
     static Scanner sc = new Scanner(System.in);
-
-    static String cheapest(String Start,String end) {
-        long min= Integer.MAX_VALUE;
+    public static String cheapest(int id , String Start,String end) {
+        long min = Integer.MAX_VALUE;
         long min1 = Integer.MAX_VALUE;
-        int flag=0;
+        int flag = 0;
 //        System.out.println(min);
         String name = null;
 
         for (Hotels hotels : hotelList) {
-            if(Start.equals(hotels.start) && end.equals(hotels.end) ) {
+            if (!arr.contains(id)) {
+                if (Start.equals(hotels.start) && end.equals(hotels.end)) {
 //                && weekstart.equals(hotels.start_week) && weekend.equals(hotels.end_week)
-                if (hotels.Regular_Price < min && hotels.week_price < min1 && flag < hotels.Rating) {
-                    min = hotels.Regular_Price;
-                    min1 = hotels.week_price;
-                    name = hotels.Name;
-                    flag = hotels.Rating;
+                    if (hotels.Regular_Price < min && hotels.week_price < min1 && flag < hotels.Rating) {
+                        min = hotels.Regular_Price;
+                        min1 = hotels.week_price;
+                        name = hotels.Name;
+                        flag = hotels.Rating;
+                    }
+                }
+                arr.add(id);
+            } else {
+                if (Start.equals(hotels.start) && end.equals(hotels.end)) {
+//                && weekstart.equals(hotels.start_week) && weekend.equals(hotels.end_week)
+                    if (hotels.srr < min && hotels.swr < min1 && flag < hotels.Rating) {
+                        min = hotels.srr;
+                        min1 = hotels.swr;
+                        name = hotels.Name;
+                        flag = hotels.Rating;
+                    }
                 }
             }
+
         }
-        return name +" "+ min;
+        return name + " " +min;
     }
-    static Boolean addHotel(String name, long regular_price,String start,String end,String start_week,String end_week,long week_price,int Rating) {
-        Hotels hot = new Hotels(name,regular_price,start,end,start_week,end_week,week_price,Rating);
+    public static Boolean addHotel(String name, long regular_price,String start,String end,String start_week,String end_week,long week_price,int Rating,long srr,long swr) {
+        Hotels hot = new Hotels(name,regular_price,start,end,start_week,end_week,week_price,Rating,srr,swr);
         hotelList.add(hot);
         if(name.equals(hot.Name) && regular_price == hot.Regular_Price) {
             return true;
@@ -77,10 +94,11 @@ public class Main {
 
     public static void main(String args[]){
         Main m = new Main();
-        m.addHotel("LakeWood",20000,"20-may-2002","21-july-2002","26-may-2002","27-may-2002",30000,3);
-        m.addHotel("BridgeWood",4000,"20-may-2002","21-july-2002","26-may-2002","27-may-2002",6000,4);
-        m.addHotel("RidgeWood",5000,"20-jan-2002","11-feb-2002","25-jan-2002","26-jan-2002",7000,5);
-
+        m.addHotel("LakeWood",110,"20-may-2002","21-july-2002","26-may-2002","27-may-2002",90,3,80,80);
+        m.addHotel("BridgeWood",150,"20-may-2002","21-july-2002","26-may-2002","27-may-2002",50,4,110,50);
+        m.addHotel("RidgeWood",220,"20-jan-2002","11-feb-2002","25-jan-2002","26-jan-2002",150,5,100,40);
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
     }
-
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -9,19 +9,21 @@ public class MainTest {
     @Test
     public void testAdd(){
 
-        Assert.assertTrue(m.addHotel("LakeWood",20000,"20-may-2002","21-july-2002","26-may-2002","27-may-2002",30000,3));
-        Assert.assertTrue(  m.addHotel("BridgeWood",4000,"20-may-2002","21-july-2002","26-may-2002","27-may-2002",6000,4));
-        Assert.assertTrue(  m.addHotel("RidgeWood",5000,"20-jan-2002","11-feb-2002","25-jan-2002","26-jan-2002",7000,5));
+        Assert.assertTrue( m.addHotel("LakeWood",110,"20-may-2002","21-july-2002","26-may-2002","27-may-2002",90,3,80,80));
+        Assert.assertTrue( m.addHotel("BridgeWood",150,"20-may-2002","21-july-2002","26-may-2002","27-may-2002",50,4,110,50));
+        Assert.assertTrue( m.addHotel("RidgeWood",220,"20-jan-2002","11-feb-2002","25-jan-2002","26-jan-2002",150,5,100,40));
+
 
     }
 
     @Test
     public void testcheapest(){
 
-        Assert.assertTrue(m.addHotel("LakeWood",20000,"20-may-2002","21-july-2002","26-may-2002","27-may-2002",30000,3));
-        Assert.assertTrue(  m.addHotel("BridgeWood",4000,"20-may-2002","21-july-2002","26-may-2002","27-may-2002",6000,4));
-        Assert.assertTrue(  m.addHotel("RidgeWood",5000,"20-jan-2002","11-feb-2002","25-jan-2002","26-jan-2002",7000,5));
-        String str = m.cheapest("20-jan-2002","11-feb-2002");
-        Assert.assertEquals("RidgeWood 5000",str);
+        Assert.assertTrue(m.addHotel("LakeWood",110,"20-may-2002","21-july-2002","26-may-2002","27-may-2002",90,3,80,80));
+        Assert.assertTrue(m.addHotel("BridgeWood",150,"20-may-2002","21-july-2002","26-may-2002","27-may-2002",50,4,110,50));
+        Assert.assertTrue(m.addHotel("RidgeWood",220,"20-jan-2002","11-feb-2002","25-jan-2002","26-jan-2002",150,5,100,40));
+        String str = m.cheapest(1,"20-jan-2002","11-feb-2002");
+        String str1 = m.cheapest(4,"20-jan-2002","11-feb-2002");
+        Assert.assertEquals("RidgeWood 100",str);
     }
 }
